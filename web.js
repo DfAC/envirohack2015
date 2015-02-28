6983ccdf5df98f38d0a6ca2a772ab31c9e18d7ca
kazlauskis <kazlauskis>
2015-02-28 01:12:01 +0000

    trying to turn on website

--- web.js
+++ web.js
@@ -1,57 +1,15 @@
-var http = require("http"),
-// utilities for working with file paths
-  path = require("path"),
-// utilities for accessing the file system
-  fs = require("fs"),
-  extensions = {
-    ".html": "text/html",
-    ".css": "text/css",
-    ".js": "application/javascript",
-    ".png": "image/png",
-    ".gif": "image/gif",
-    ".jpg": "image/jpeg",
-    ".ttf": "font/truetype",
-    ".otf": "font/opentype",
-    ".woff": "application/x-font-woff"
-  };
+var express = require('express');
+var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-http.createServer(function(req, res) {
+var server = app.listen(3000, function () {
 
-// look for a filename in the URL, default to index.html
-  var filename = path.basename(req.url) || "index.html",
-    ext = path.extname(filename),
-    dir = path.dirname(req.url).substring(1),
-// __dirname is a built-in variable containing the path where the code is running
-    localPath = __dirname + "/assets/";
-  if (extensions[ext]) {
-    localPath += (dir ? dir + "/" : "") + filename;
-    fs.exists(localPath, function(exists) {
-      if (exists) {
-        getFile(localPath, extensions[ext], res);
-      } else {
-        res.writeHead(404);
-        res.end();
-      }
-    });
-  }
+  var host = server.address().address;
+  var port = server.address().port;
 
-  function getFile(localPath, mimeType, res) {
-    fs.readFile(localPath, function(err, contents) {
-      if (!err) {
-        res.writeHead(200, {
-          "Content-Type": mimeType,
-          "Content-Length": contents.length
-        });
-        res.end(contents);
-      } else {
-        res.writeHead(500);
-        res.end();
-      }
-    });
-  }
+  console.log('Example app listening at http://%s:%s', host, port);
 
-
-}).listen(process.env.PORT || 3000, function() {
-  console.log("listening on 3000");
 });
